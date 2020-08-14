38631ec1570c3b000f9a96540a3193fd8541cbd7
Bilgin COSKUN <bilgin.coskun@ceng.metu.edu.tr>
2020-08-14 14:36:28 +0300

    Notify Wrong Character Input in Relabeling

--- src/brogue/Items.c
+++ src/brogue/Items.c
@@ -5881,6 +5881,9 @@ void relabel(item *theItem) {
                     theItem->inventoryLetter);
             messageWithColor(buf, &itemMessageColor, false);
         }
+    }else{
+        sprintf(buf, "Entered label is not a letter");
+        messageWithColor(buf, &itemMessageColor, false);
     }
 }
 
