41c90cc15f634f8caa3bf6ad7520cdfcfd93cc75
Bilgin COSKUN <bilgin.coskun@ceng.metu.edu.tr>
2020-08-19 17:15:42 +0300

    Change High Score Continue Message

--- src/brogue/IO.c
+++ src/brogue/IO.c
@@ -4434,8 +4434,10 @@ void printHighScores(boolean hiliteMostRecent) {
     scoreColor = black;
     applyColorAverage(&scoreColor, &goodMessageColor, 100);
 
-    printString(KEYBOARD_LABELS ? "Press space to continue." : "Touch anywhere to continue.",
-                (COLS - strLenWithoutEscapes(KEYBOARD_LABELS ? "Press space to continue." : "Touch anywhere to continue.")) / 2,
+    char highScoreMessage[] = "Touch anywhere to continue.";
+    
+	printString(highScoreMessage,
+                (COLS - strLenWithoutEscapes(highScoreMessage)) / 2,
                 ROWS - 1, &scoreColor, &black, 0);
 
     commitDraws();
