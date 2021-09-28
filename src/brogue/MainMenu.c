dd73c1158827968bec4521aed00137e90383e0be
Bilgin COSKUN <bilgin.coskun@ceng.metu.edu.tr>
2021-09-28 09:10:18 +0300

    Move tryParseUint64

--- src/brogue/MainMenu.c
+++ src/brogue/MainMenu.c
@@ -641,6 +641,20 @@ boolean dialogChooseFile(char *path, const char *suffix, const char *prompt) {
     }
 }
 
+boolean tryParseUint64(char *str, uint64_t *num) {
+    unsigned long long n;
+    char buf[100];
+    if (strlen(str)                 // we need some input
+        && sscanf(str, "%llu", &n)  // try to convert to number
+        && sprintf(buf, "%llu", n)  // convert back to string
+        && !strcmp(buf, str)) {     // compare (we need them equal)
+        *num = (uint64_t)n;
+        return true; // success
+    } else {
+        return false; // input was too large or not a decimal number
+    }
+}
+
 // This is the basic program loop.
 // When the program launches, or when a game ends, you end up here.
 // If the player has already said what he wants to do next
